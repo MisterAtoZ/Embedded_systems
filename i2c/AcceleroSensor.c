22e55a2ca1c3dc3724a12236261ed5c90f38affb
MisterAtoZ <jonathan.luijsmans@student.uhasselt.be>
2019-01-07 17:26:53 +0100

    werken aan ic driver

---
+++ i2c/AcceleroSensor.c
@@ -0,0 +1,232 @@
+#include "AcceleroSensor.h"
+#include <stdio.h>
+#include <math.h>
+struct Vector_Float d;
+struct Vector_Float t;
+struct Vector_Short r;
+struct Vector_Float n;
+
+unsigned int useCalibrate;
+unsigned int actualThreshold;
+float dpsPerDigit;
+float thresholdX;
+float thresholdY;
+float thresholdZ;
+
+int L3G4200D_begin(l3g4200d_dps_t scale, l3g4200d_odrbw_t odrbw) {
+    d.x = 0;
+    d.y = 0;
+    d.z = 0;
+    useCalibrate = 0;
+
+    t.x = 0;
+    t.y = 0;
+    t.z = 0;
+    actualThreshold = 0;
+
+    if (L3G4200D_fastRegister(SENS_WHO_AM_I) != 0xD3) { //adress aanpassen (missch toch)!
+        return 0;
+    }
+
+    unsigned int reg1 = 0x00;
+    reg1 |= 0x0F;
+    reg1 |= (odrbw << 4);
+
+    writeRegister(SENS_CTRL_REG5_XL, 0b01111000);//update every 2 sample, accelerometer activated on all axis //inputs the value of what the register should be (p51)
+    writeRegister(SENS_CTRL_REG6_XL, 0b00000000); //All on default values
+    writeRegister(SENS_CTRL_REG7_XL, 0b00000000); //No filters are added
+    writeRegister(SENS_CTRL_REG8, 0b00000100); //More settings
+    writeRegister(SENS_CTRL_REG9, 0b00000010); //FIFO memory enabled
+    writeRegister(SENS_CTRL_REG10, 0b00000000); //self test disabled
+
+/*
+    L3G4200D_writeRegister(L3G4200D_REG_CTRL_REG1, reg1);
+    L3G4200D_writeRegister(L3G4200D_REG_CTRL_REG2, 0x00);
+    L3G4200D_writeRegister(L3G4200D_REG_CTRL_REG3, 0x08);
+    L3G4200D_writeRegister(L3G4200D_REG_CTRL_REG4, scale << 4);
+*/
+ /*   switch (scale) {
+        case L3G4200D_SCALE_250DPS:
+            dpsPerDigit = .00875f; //114.28
+            break;
+        case L3G4200D_SCALE_500DPS:
+            dpsPerDigit = .0175f; //57.14
+            break;
+        case L3G4200D_SCALE_2000DPS:
+            dpsPerDigit = .07f; //14.28
+            break;
+        default:
+            break;
+    }*/
+
+    //L3G4200D_writeRegister(L3G4200D_REG_CTRL_REG5, 0x00);
+
+    return 1;
+}
+/*
+l3g4200d_dps_t L3G4200D_getScale() {
+    return (l3g4200d_dps_t)((L3G4200D_readRegister(L3G4200D_REG_CTRL_REG4) >> 4) & 0x03);
+}
+
+l3g4200d_odrbw_t L3G4200D_getOdrBw() {
+    return (l3g4200d_odrbw_t)((L3G4200D_readRegister(L3G4200D_REG_CTRL_REG1) >> 4) & 0x0F);
+}*/
+
+
+// float sqroot(float square) {
+//     float root=square/3;
+//     int i;
+//     if (square <= 0) return 0;
+//     for (i=0; i<32; i++)
+//         root = (root + square / root) / 2;
+//     return root;
+// }
+
+struct Vector_Short readData() {
+    signed int dataX[] = {(SENS_OUT_X_XL1 | (1 << 7))};
+    signed int buffer[6];
+
+    write_bytes(SENS_SLAVE_ADRESS, data, 1);
+
+    read_bytes(SENS_SLAVE_ADRESS, buffer, 6);
+
+    r.x = (buffer[1] << 8 | buffer[0]) & 0xFFFF;
+    r.y = (buffer[3] << 8 | buffer[2]) & 0xFFFF;
+    r.z = (buffer[5] << 8 | buffer[4]) & 0xFFFF;
+
+    return r;
+}
+
+//===============================================================================================
+
+void L3G4200D_calibrate(unsigned int samples) {
+    useCalibrate = 1;
+
+    float sumX = 0;
+    float sumY = 0;
+    float sumZ = 0;
+    float sigmaX = 0;
+    float sigmaY = 0;
+    float sigmaZ = 0;
+
+    for (unsigned int i = 3; i > 0; i--) {
+        uprintf("calibrating in: %d seconds, please hold still\n\r", i);
+        delay_ms(1000);
+    }
+
+    for (unsigned int i = 0; i < samples; i++) {
+        L3G4200D_readRaw();
+
+        sumX += r.x;
+        sumY += r.y;
+        sumZ += r.z;
+
+        sigmaX += r.x * r.x;
+        sigmaY += r.y * r.y;
+        sigmaZ += r.z * r.z;
+
+        delay_ms(20);
+    }
+
+    d.x = sumX / samples;
+    d.y = sumY / samples;
+    d.z = sumZ / samples;
+
+    thresholdX = ((sigmaX / samples) - (d.x * d.x));
+    thresholdY = ((sigmaY / samples) - (d.y * d.y));
+    thresholdZ = ((sigmaZ / samples) - (d.z * d.z));
+
+    uprintf("thresholdX %d\n\r", (int) thresholdX);
+
+    if (actualThreshold > 0) {
+        L3G4200D_setThreshold(actualThreshold);
+    }
+}
+
+
+
+unsigned int L3G4200D_getThreshold() {
+    return actualThreshold;
+}
+
+void L3G4200D_setThreshold(unsigned int multiple) {
+    if (multiple > 0) {
+        if (!useCalibrate) {
+            uprintf("calibrating in set threshold\n\r");
+            L3G4200D_calibrate(40);
+        }
+
+        t.x = thresholdX * multiple*multiple;
+        t.y = thresholdY * multiple*multiple;
+        t.z = thresholdZ * multiple*multiple;
+
+        uprintf("tx: %d\n\r", (int)t.x);
+    } else {
+        t.x = 0;
+        t.y = 0;
+        t.z = 0;
+    }
+
+    actualThreshold = multiple;
+}
+
+void L3G4200D_writeRegister(unsigned int reg, unsigned int value) {
+    unsigned int data[] = {reg, value};
+    write_bytes(SENS_SLAVE_ADRESS, data, 2);
+}
+
+unsigned int L3G4200D_readRegister(unsigned int reg) {
+    unsigned int value;
+    unsigned int buffer[1];
+    unsigned int data[] = {reg};
+
+    write_bytes(SENS_SLAVE_ADRESS, data, 1);
+    read_bytes(SENS_SLAVE_ADRESS, buffer, 1);
+    value = buffer[0];
+    return value;
+}
+
+unsigned int L3G4200D_fastRegister(unsigned int reg) {
+    unsigned int buffer[1];
+    unsigned int data[] = {reg};
+    write_bytes(SENS_SLAVE_ADRESS, data, 1);
+    read_bytes(SENS_SLAVE_ADRESS, buffer, 1);
+    return buffer[0];
+}
+
+struct Vector_Short L3G4200D_readRaw() {
+    unsigned int data[] = {(L3G4200D_REG_OUT_X_L | (1 << 7))};
+    unsigned int buffer[6];
+
+    write_bytes(SENS_SLAVE_ADRESS, data, 1);
+
+    read_bytes(SENS_SLAVE_ADRESS, buffer, 6);
+
+    r.x = (buffer[1] << 8 | buffer[0]) & 0xFFFF;
+    r.y = (buffer[3] << 8 | buffer[2]) & 0xFFFF;
+    r.z = (buffer[5] << 8 | buffer[4]) & 0xFFFF;
+
+    return r;
+}
+
+struct Vector_Float L3G4200D_readNormalize() {
+    L3G4200D_readRaw();
+
+    if (useCalibrate) {
+        n.x = (r.x - d.x) * dpsPerDigit;
+        n.y = (r.y - d.y) * dpsPerDigit;
+        n.z = (r.z - d.z) * dpsPerDigit;
+    } else {
+        n.x = r.x * dpsPerDigit;
+        n.y = r.y * dpsPerDigit;
+        n.z = r.z * dpsPerDigit;
+    }
+
+    if (actualThreshold > 0) {
+        if (n.x*n.x < t.x*dpsPerDigit*dpsPerDigit) n.x = 0;
+        if (n.y*n.y < t.y*dpsPerDigit*dpsPerDigit) n.y = 0;
+        if (n.z*n.z < t.z*dpsPerDigit*dpsPerDigit) n.z = 0;
+    }
+
+    return n;
+}
---
+++ i2c/defines.h
@@ -0,0 +1,125 @@
+/********************************************************************
+Copyright 2010-2017 K.C. Wang, <kwang@eecs.wsu.edu>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+NOTE: This header file is modified from its original contents to 
+	  facilitate the implementation of an interrupt-driven UART
+	  driver.
+********************************************************************/
+
+// defines.h file
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+// General type definitions
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+// GPIO 
+#define GPIO_BASE	0x20200000
+
+#define GPIO_GPFSEL0    0
+#define GPIO_GPFSEL1    1
+#define GPIO_GPFSEL2    2
+#define GPIO_GPFSEL3    3
+#define GPIO_GPFSEL4    4
+#define GPIO_GPFSEL5    5
+
+#define GPIO_GPSET0     7
+#define GPIO_GPSET1     8
+
+#define GPIO_GPCLR0     10
+#define GPIO_GPCLR1     11
+
+#define GPIO_GPLEV0     13
+#define GPIO_GPLEV1     14
+
+#define GPIO_GPEDS0     16
+#define GPIO_GPEDS1     17
+
+#define GPIO_GPREN0     19
+#define GPIO_GPREN1     20
+
+#define GPIO_GPFEN0     22
+#define GPIO_GPFEN1     23
+
+#define GPIO_GPHEN0     25
+#define GPIO_GPHEN1     26
+
+#define GPIO_GPLEN0     28
+#define GPIO_GPLEN1     29
+
+#define GPIO_GPAREN0    31
+#define GPIO_GPAREN1    32
+
+#define GPIO_GPAFEN0    34
+#define GPIO_GPAFEN1    35
+
+#define GPIO_GPPUD      37
+#define GPIO_GPPUDCLK0  38
+#define GPIO_GPPUDCLK1  39
+
+// Peripheral base address
+#define PERIPHERAL_BASE 0x20000000UL
+
+// UART
+#define UDR   0x00
+#define UDS   0x04
+#define RXFE  0x10
+#define UFR   0x18
+#define TXFF  0x20
+#define CNTL  0x2C
+#define IMSC  0x38
+#define MIS   0x40
+#define SBUFSIZE 128
+
+// I2C
+#define BSC1_BASE (0x20804000)
+
+#define BSC1_C        0
+#define BSC1_S        1
+#define BSC1_DLEN     2
+#define BSC1_A        3
+#define BSC1_FIFO     4
+#define CLOCK_DIVIDER 5
+#define DATA_DELAY	  6
+#define CLOCK_STRETCH_TIMEOUT 7
+
+#define BSC_C_I2CEN   (1 << 15)
+#define BSC_C_INTR    (1 << 10)
+#define BSC_C_INTT    (1 << 9)
+#define BSC_C_INTD    (1 << 8)
+#define BSC_C_ST      (1 << 7)
+#define BSC_C_CLEAR   (1 << 4)
+#define BSC_C_READ    1
+
+#define START_READ    BSC_C_I2CEN|BSC_C_ST|BSC_C_CLEAR|BSC_C_READ
+#define START_WRITE    BSC_C_I2CEN|BSC_C_ST
+
+#define BSC_S_CLKT   (1 << 9)
+#define BSC_S_ERR    (1 << 8)
+#define BSC_S_RXF    (1 << 7)
+#define BSC_S_TXE    (1 << 6)
+#define BSC_S_RXD    (1 << 5)
+#define BSC_S_TXD    (1 << 4)
+#define BSC_S_RXR    (1 << 3)
+#define BSC_S_TXW    (1 << 2)
+#define BSC_S_DONE   (1 << 1)
+#define BSC_S_TA     1
+
+#define CLEAR_STATUS    BSC_S_CLKT|BSC_S_ERR|BSC_S_DONE
+
+#endif
